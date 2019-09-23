26c2d90548d5fcae86ad7508f87c05be40ee44b2
谢荣锋 <Xierongfeng@outlook.com>
2019-09-23 12:59:47 +0800

    Update toc.js

--- public/js/toc.js
+++ public/js/toc.js
@@ -3,7 +3,7 @@
     $.fn.toc = function (options) {
         var defaults = {
                 noBackToTopLinks: false,
-                title: '<i>目录</i>',
+                title: '<i class="zero-title text-center ">目录</i>',
                 minimumHeaders: 3,
                 headers: 'h1, h2, h3, h4, h5, h6',
                 listType: 'ul', // values: [ol|ul]
